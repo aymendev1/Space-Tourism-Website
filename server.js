7074efa781de5e5997d7a4c8132916cf61ace14e
Ayman Azougar <topayman5@gmail.com>
2022-04-16 18:33:49 +0200

    Upload Data From json file to destination Page

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const ejs = require("ejs");
+// require data from json file
+const data = require("./data.json");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
@@ -13,6 +15,10 @@ app.get("/destination", (req, res) => {
   const csslinkD = "css/destination-styles.css";
   res.render("destination", { CSSLINK: csslinkD });
 });
+app.get("/destination/data", (req, res) => {
+  const DestinationData = data.destinations;
+  res.send(DestinationData);
+});
 app.get("/crew", (req, res) => {
   const csslinkC = "css/crew-styles.css";
   res.render("crew", { CSSLINK: csslinkC });
@@ -20,6 +26,12 @@ app.get("/crew", (req, res) => {
 app.get("/technology", (req, res) => {
   const csslinkT = "css/Technology-styles.css";
   res.render("technology", { CSSLINK: csslinkT });
+});
+app.get("/data", (req, res) => {
+  let response = fetch("../data.json");
+  let data = response.text();
+  console.log(data);
+  res.send(data);
 });
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
